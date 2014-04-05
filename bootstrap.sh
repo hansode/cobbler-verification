855affad1d1e6010e55948d65f5676f7247aa27d
Masahito Yoshida <masahito@axsh.net>
2014-04-05 18:15:30 +0900

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+function yum() {
+  $(type -P yum) --disablerepo=updates "${@}"
+}
+
+# Add installation packages ...
+addpkgs="
+  fence-agents
+"
+
+if [[ -n "$(echo ${addpkgs})" ]]; then
+  yum install -y ${addpkgs}
+fi
+
+rpm -qi epel-release-6-8 >/dev/null || { yum install --disablerepo=updates -y http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm; }
+yum install -y cobbler
