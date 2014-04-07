9a405706ca21d035687c352c41ac706d383a7a0e
Masahito Yoshida <masahito@axsh.net>
2014-04-07 21:25:03 +0900

    update cobller signature.

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,3 +22,4 @@ fi
 
 rpm -qi epel-release-6-8 >/dev/null || { yum install --disablerepo=updates -y http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm; }
 yum install -y cobbler
+cobbler signature update
